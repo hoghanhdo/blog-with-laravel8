4ca4ce017de8fd7c1e9223faad244938091fbc00
hanh-do <hoghanhdo@gmail.com>
2022-02-06 00:45:40 +0900

    Update factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,25 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Truncate data before seeding
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory()->create();
-
-        $category = Category::create([
-            'name' => 'Travel',
-            'slug' => 'travel'
+        // No need for truncation if php artisan migrate:fresh --seed
+        $user1 = User::factory()->create([
+            'name' => 'Jane Doe'
         ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $category->id,
-            'slug' => 'thailand',
-            'title' =>  "10 places to visit in Thailand",
-            'excerpt' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo assumenda rerum tempore beatae laborum dolore modi id, neque obcaecati atque placeat reprehenderit ea, praesentium doloribus recusandae earum accusantium quia dolorem.",
-            'body' => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut modi dolore quam eos accusamus exercitationem neque provident recusandae, mollitia, dolorum impedit molestias blanditiis cupiditate dolorem! Ratione delectus quisquam eos eveniet!",
+        $user2 = User::factory()->create([
+            'name' => 'Eddie M'
+        ]);
+        Post::factory(3)->create([
+            'user_id' => $user1->id
+        ]);
+        Post::factory(3)->create([
+            'user_id' => $user2->id
         ]);
     }
 }
