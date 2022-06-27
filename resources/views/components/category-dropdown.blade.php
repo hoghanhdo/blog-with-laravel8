b35adfaff8c64d9d193ff90289fc3f308bdff83d
hanh-do <hoghanhdo@gmail.com>
2022-06-27 22:53:00 +0900

    Merge filtering

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -20,7 +20,7 @@
     class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-gray-300"
     >All</a>
     @foreach ($categories as $category)
-            <a href="/?category={{ $category->slug }}"
+            <a href="/?category={{ $category->slug }}&{{ http_build_query( request()->except('category') )}}"
             class="block text-left px-3 text-sm leading-6 hover:bg-gray-300 focus:bg-gray-300
             {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-gray-300' : '' }}"
             >{{ ucwords($category->name) }}</a>
