182d44dc68f8978ecd08067af72d65e4adaf7fa9
hanh-do <hoghanhdo@gmail.com>
2022-07-15 15:13:28 +0900

    Update eloquent on models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,6 +21,11 @@ class Post extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
     public function scopeFilter($query, array $filters) // Post::newQuery->filter()
     {
         $query->when($filters['search'] ?? false, function($query, $search) {
